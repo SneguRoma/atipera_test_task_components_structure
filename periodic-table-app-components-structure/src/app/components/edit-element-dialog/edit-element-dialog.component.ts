1c4757eb356f0e40e61e20a067136590c1500caf
sneguroma <sneguroma@gmail.com>
2024-09-16 13:06:00 +0200

    feat: project migrated to components structure

---
+++ periodic-table-app-components-structure/src/app/components/edit-element-dialog/edit-element-dialog.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { PeriodicElement } from '../../data';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-edit-element-dialog',
+  templateUrl: './edit-element-dialog.component.html',
+  styleUrl: './edit-element-dialog.component.css',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+})
+export class EditElementDialogComponent {
+  readonly dialogRef = inject(MatDialogRef<EditElementDialogComponent>);
+  readonly data = inject<PeriodicElement>(MAT_DIALOG_DATA);
+  dataCopy!: PeriodicElement;
+
+  constructor() {
+    this.dataCopy = { ...this.data };
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
