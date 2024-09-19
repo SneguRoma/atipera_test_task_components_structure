9d39b6b9b299e05ed5d68b7f1846bb75770d60c2
sneguroma <sneguroma@gmail.com>
2024-09-19 14:00:34 +0200

    feat: DataLoadingService refactored to rx-angular state

--- periodic-table-app-components-structure/src/app/services/data-loading.service.ts
+++ periodic-table-app-components-structure/src/app/services/data-loading.service.ts
@@ -1,30 +1,36 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { Injectable, Signal } from '@angular/core';
 import { ELEMENT_DATA, PeriodicElement } from '../data';
 import { Observable, of, delay } from 'rxjs';
+import { rxState } from '@rx-angular/state';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataLoadingService {
-  private elementsSignal = signal<PeriodicElement[]>([]);
+  private state = rxState<{ elements: PeriodicElement[] }>(({ set }) => {
+    set({ elements: [] });
+  });
+  private elementsSignal = this.state.signal('elements');
 
   loadElements(): Observable<PeriodicElement[]> {
     return of(ELEMENT_DATA).pipe(delay(1000));
   }
 
-  getElements(): WritableSignal<PeriodicElement[]> {
+  getElements(): Signal<PeriodicElement[]> {
+    console.log('get');
     return this.elementsSignal;
   }
 
   setElements(elements: PeriodicElement[]): void {
-    this.elementsSignal.set(elements);
+    this.state.set({ elements });
   }
 
   updateElements(updateElement: PeriodicElement): void {
-    return this.elementsSignal.update((elements) => {
-      return elements.map((element) =>
+    this.state.set('elements', ({ elements }) =>
+      elements.map((element) =>
         element.position === updateElement.position ? updateElement : element,
-      );
-    });
+      ),
+    );
+    console.log('updateElement', updateElement);
   }
 }
